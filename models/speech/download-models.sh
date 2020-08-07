5ccb753338018b45829b5d72eca115ecdbbd1b0c
lnxusr1 <lnxusr1@neo>
2020-08-06 21:54:21 -0400

    Upgraded to deepspeech v0.8.0 and related models.

--- models/speech/download-models.sh
+++ models/speech/download-models.sh
@@ -3,6 +3,6 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $DIR
 
-wget -c https://github.com/mozilla/DeepSpeech/releases/download/v0.7.4/deepspeech-0.7.4-models.pbmm
-wget -c https://github.com/mozilla/DeepSpeech/releases/download/v0.7.4/deepspeech-0.7.4-models.tflite
-wget -c https://github.com/mozilla/DeepSpeech/releases/download/v0.7.4/deepspeech-0.7.4-models.scorer
+wget -c https://github.com/mozilla/DeepSpeech/releases/download/v0.8.0/deepspeech-0.8.0-models.pbmm
+wget -c https://github.com/mozilla/DeepSpeech/releases/download/v0.8.0/deepspeech-0.8.0-models.tflite
+wget -c https://github.com/mozilla/DeepSpeech/releases/download/v0.8.0/deepspeech-0.8.0-models.scorer
